dd9bc7eaee1cdb63cfe2b81a1f5daf4becf445f0
taphu987 <phu.tn1309@gmail.com>
2023-02-09 13:55:53 +0700

    Handle state change on Column & Card

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'font-awesome/css/font-awesome.min.css';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
@@ -7,7 +9,7 @@ ReactDOM.render(
     <React.StrictMode>
         <App />
     </React.StrictMode>,
-    document.getElementById('root'),
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
